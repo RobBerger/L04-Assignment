db138b860435eb0810aa1c3973bb99860a440807
berger <robertberger.dev@gmail.com>
2022-12-20 20:06:40 -0500

    accomplished objective

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var smallerBtn = document.getElementById('decreaseBtn');
 var barWidth = document.getElementById('rectangle').style.width;
 var barWidthNum = parseInt(barWidth);
 smallerBtn.addEventListener('click', () => {
-    console.log(barWidth);
-    console.log(barWidthNum);
+    barWidthNum = barWidthNum - 10;
+    document.getElementById('rectangle').style.width = barWidthNum + 'px';
 });
