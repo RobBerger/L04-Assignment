c9360e0afd344909a2dc08e18da20a839ebe78a8
berger <robertberger.dev@gmail.com>
2022-12-20 19:49:42 -0500

    stored width value as var

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var smallerBtn = document.getElementById('decreaseBtn');
-
+var barWidth = document.getElementById('rectangle').style.width;
+var barWidthNum = parseInt(barWidth);
 smallerBtn.addEventListener('click', () => {
-
+    console.log(barWidth);
+    console.log(barWidthNum);
 });
