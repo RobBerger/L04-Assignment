1739b1e1137e96c13c63936c3f839b8799167dc4
berger <robertberger.dev@gmail.com>
2022-12-20 20:14:47 -0500

    made the increase button work

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var smallerBtn = document.getElementById('decreaseBtn');
+var biggerBtn = document.getElementById('increaseBtn');
 var barWidth = document.getElementById('rectangle').style.width;
 var barWidthNum = parseInt(barWidth);
+
 smallerBtn.addEventListener('click', () => {
     barWidthNum = barWidthNum - 10;
+    document.getElementById('rectangle').style.width = barWidthNum + 'px';
+});
+
+biggerBtn.addEventListener('click', () => {
+    barWidthNum = barWidthNum + 10;
     document.getElementById('rectangle').style.width = barWidthNum + 'px';
 });
